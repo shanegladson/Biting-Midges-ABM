22f93d11305cfe7979b72a5f3bca0f47ac145982
Shane Gladson <shanegladson@gmail.com>
2021-02-16 23:32:02 -0500

    Add files via upload

--- Model (WIP)/Midge.py
+++ Model (WIP)/Midge.py
@@ -7,23 +7,39 @@ import random
 import numpy as np
 
 class Walker(Agent):
-    age = 0 # Age in days
+    dps = 0.7 # Daily probability of survival. Applied every day of midge's life
     fed = False # True if the midge has taken a bloodmeal recently, False otherwise
     timesincefed = 0 # Time in days since the midge has fed, will only increase if the midge has fed at all, resets to 0 after another bloodmeal
     avgeggbatch = 100 # Average number of eggs laid per oviposition
     gtrclength = 14 # Number of days in the gonotrophpic cycle
     senserange = 5 # Range at which midges can detect CO2 emitting from source (deer or trap)
 
-    def __init__(self, unique_id, model):
+
+    def __init__(self, unique_id, model, x, y):
         super().__init__(unique_id, model)
-        
+        self.x, self.y = x, y
+
     def step(self):
-        self.move()
+        if True:
+            # In future add toggle between random walk (searching) and biased walk (locked on to CO2 emitter)
+            self.randomwalk()
+            self.x, self.y = self.pos
+        # TODO: Implement bloodfeeding/oviposition
+
+        # Daily probability of survival calculation (uniform distribution), calculated at end of step function
+        if random.random() > self.dps:
+            self.death()
     
+    # Random walk function, chooses randomly from available cells in Moore neighborhood
     def randomwalk(self):
         possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True)
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
-
-    def biasedwalk(self):
+ 
+    # TODO: implement biased walk method when attracted to CO2 source (emission point)
+    def biasedwalk(self, pos):
         return
+
+    def death(self):
+        self.model.grid._remove_agent(self.pos, self)
+        self.model.schedule.remove(self)
--- Model (WIP)/World.py
+++ Model (WIP)/World.py
@@ -1,29 +1,26 @@
 from mesa import Model
-from mesa.space import ContinuousSpace
+from mesa.space import MultiGrid
 from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 from mesa.batchrunner import BatchRunner
 import random
 import numpy as np
-import Midge
+from Midge import Walker
 
 class WorldModel(Model):
-    def __init__(self, N, width=1000, height=1000, start=None):
+    def __init__(self, N, width=100, height=100):
         self.NumAgents = N
         self.schedule = RandomActivation(self)
         self.grid = MultiGrid(width, height, True)
         for i in range(self.NumAgents):
-            a = Walker(i, self)
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            a = Walker(i, self, x, y)
             self.schedule.add(a)
             
-            if start == None:
-                x = self.random.randrange(self.grid.width)
-                y = self.random.randrange(self.grid.height)
-                self.grid.place_agent(a, (x,y))
-            else:
-                self.grid.place_agent(a, start)
-            
-        self.datacollector = DataCollector(agent_reporters={"Position" : "pos"})
+            self.grid.place_agent(a, (x,y))
+
+        self.datacollector = DataCollector(agent_reporters={"x" : "x", "y" : "y"})
 
     def step(self):
         self.datacollector.collect(self)
